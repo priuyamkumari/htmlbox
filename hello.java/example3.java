21aca262a0226f615acb0565790c97681798ca61
priyam kumari <priyamkumaripipra@gmil.com>
2024-09-27 23:14:52 +0530

    first program

---
+++ hello.java/example3.java
@@ -0,0 +1,11 @@
+ import java.uti.*;
+  public class first{
+    public static void main(strin[]srgs){
+        int age =sc.nextInt();
+        if(age>18){
+            System.out.println("adult");
+        }else{
+            System.out.println("not adult");
+        }
+    }
+}
---
+++ hello.java/example4.java
@@ -0,0 +1,7 @@
+class basics;
+
+public class example4.java {
+	public static void main(String[] args) {
+        System.out,println("hello");
+    }
+}
